75c8e9ca58ae73282199e7dcee1b08eca37f4047
TheMightyd7 <dd@davisinfo.com>
2021-03-04 09:31:35 -0500

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,8 @@
 # This init is required for each cog.
 # Import your main class from the cog's folder.
-from .test import TestCog
+from .test import Test
 
 
 def setup(bot):
     # Add the cog to the bot.
-    bot.add_cog(TestCog())
+    bot.add_cog(Test())
